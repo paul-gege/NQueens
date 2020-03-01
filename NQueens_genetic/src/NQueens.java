421f904358376785080201ed2902588e9527e45c
paul-gege <55391943+paul-gege@users.noreply.github.com>
2020-03-01 14:07:02 -0500

    Add files via upload

--- NQueens_genetic/src/NQueens.java
+++ NQueens_genetic/src/NQueens.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class NQueens {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Chromosome[] generation = new Chromosome[50];
+		ArrayList<String> solutions = new ArrayList<String>();
+		int count = 0;
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter the number of Queens:");
+		int myint = keyboard.nextInt();
 		
 		for(int i = 0; i < generation.length; i++) {
-			generation[i] = new Chromosome(8);
+			generation[i] = new Chromosome(myint);
 		}
 		
-		int generations = 3000*8;
+		int generations = 3000*myint;
 		
 		while(generations > 0) {
 			for(int j = 0; j < generation.length; j++) {
@@ -27,14 +36,31 @@ public class NQueens {
 								
 				//System.out.println("After crossover & mutation" + generation[j].get_fitness());
 				if(generation[j].get_fitness() == 0) {
-					System.out.println(" ");	
-					for(int num = 0; num < generation[j].board_size; num++) {
-						System.out.print(generation[j].chromosomes[num]);	
+					count++;
+					if(solutions.contains(Arrays.toString(generation[j].chromosomes))) {
+						continue;
+					}
+					else {
+						
+						for(int row = 0; row < generation[j].chromosomes.length; row++) {
+							for(int column = 0; column < generation[j].chromosomes.length; column++) {
+								if(column != generation[j].chromosomes[row]) {
+									System.out.print("   |");
+								} else {
+									System.out.print(" * |");
+								}
+							}
+							System.out.println("");
+						}
+						
+						System.out.println(Arrays.toString(generation[j].chromosomes));
+						solutions.add(Arrays.toString(generation[j].chromosomes));
 					}
 				}
 			}
 			generations--;
-		}		
+		}	
+		System.out.println("Number of solutions: " + solutions.size());
 		
 	}
 
